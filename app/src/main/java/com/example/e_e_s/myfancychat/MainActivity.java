775f28f73feba9db9717d1479f838b5008ec8a63
SondreFjaertoft <sondre.fjaertoft@hotmail.com>
2016-08-26 13:05:01 +0200

    all the shit

---
+++ app/src/main/java/com/example/e_e_s/myfancychat/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.e_e_s.myfancychat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ImageButton sendButton = (ImageButton)findViewById(R.id.imageButton);
+        //sendButton.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Toast.makeText(getApplicationContext(), "Heia verden", Toast.LENGTH_LONG).show();
+        //    }
+        //});
+    }
+
+    private EditText writeText;
+    private TextView textView1;
+
+
+
+    public void buttonOnClick(View v)
+    {
+        Button button = (Button) v;
+        //button.setOnClickListener(new View.OnClickListener());
+        writeText = (EditText) findViewById(R.id.editText1);
+        textView1 =  (TextView) findViewById(R.id.textView1);
+        textView1.setText(writeText.getText());
+    }
+
+}
